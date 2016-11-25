6255d012353b9634c64e028cac545c21f39e617f
Aivis Abele <aabele@gmail.com>
2016-11-25 23:02:03 +0200

    Added compensa scraper.

--- scrapers/base.py
+++ scrapers/base.py
@@ -40,9 +40,18 @@ class OCTAScraper(object):
     passport_id_xpath = ''
     submit_button_xpath = ''
     accept_terms_xpath = ''
+    extra_field_xpath = ''
+
+    extra_field_value = ''
+
+    change_trigger_key = Keys.ENTER
 
     def __init__(self, car_id, passport_id):
-        """ """
+        """
+
+        :param car_id:
+        :param passport_id:
+        """
         self.car_id = car_id
         self.passport_id = passport_id
 
@@ -65,6 +74,9 @@ class OCTAScraper(object):
         """
         time.sleep(self.wait_time)
 
+    def _scroll_to(self, element):
+        self.driver.execute_script("return arguments[0].scrollIntoView();", element)
+
     def _fill(self, field_xpath, value):
         """
         Find the input field and fill it with specific contents
@@ -73,8 +85,9 @@ class OCTAScraper(object):
         :param value: field value
         """
         input_field = self.driver.find_element_by_xpath(field_xpath)
+        self._scroll_to(input_field)
         input_field.send_keys(value)
-        input_field.send_keys(Keys.ENTER)
+        input_field.send_keys(self.change_trigger_key)
 
     def fill_car_id(self):
         """
@@ -90,22 +103,32 @@ class OCTAScraper(object):
         if self.passport_id_xpath:
             self._fill(self.passport_id_xpath, self.passport_id)
 
+    def fill_extra_field(self):
+        """
+
+        :return:
+        """
+        if self.extra_field_xpath:
+            self._fill(self.extra_field_xpath, self.extra_field_value)
+
     def accept_terms(self):
         """
         Find accept terms checkbox and check it
         """
         if self.accept_terms_xpath:
             input_field = self.driver.find_element_by_xpath(self.accept_terms_xpath)
+            self._scroll_to(input_field)
             input_field.click()
 
     def submit_search_form(self):
         """
         Find form submit button and click it
         """
-        submit_button = self.driver.find_element_by_xpath(self.submit_button_xpath)
-        submit_button.click()
-
-        self.wait_for_ajax()
+        if self.submit_button_xpath:
+            submit_button = self.driver.find_element_by_xpath(self.submit_button_xpath)
+            self._scroll_to(submit_button)
+            submit_button.click()
+            self.wait_for_ajax()
 
     def parse_results(self):
         raise NotImplementedError
@@ -119,6 +142,7 @@ class OCTAScraper(object):
         self.fill_car_id()
         self.fill_passport_id()
         self.accept_terms()
+        self.fill_extra_field()
         self.submit_search_form()
         self.wait_for_ajax()
         return self.parse_results()
---
+++ scrapers/providers/compensa.py
@@ -0,0 +1,39 @@
+# -*- coding: utf8 -*-
+"""
+Compensa website scraper
+"""
+
+from __future__ import unicode_literals
+
+import random
+
+from scrapers.base import OCTAScraper
+from selenium.webdriver.common.keys import Keys
+
+
+class CompensaOCTAScraper(OCTAScraper):
+    """
+    Compensa scraper class
+    """
+
+    base_url = 'http://online.compensa.lv/lv/Octa'
+    car_id_xpath = '//input[@name="PlateNumber"]'
+    passport_id_xpath = '//input[@name="CertNumber"]'
+    submit_button_xpath = '//input[@type="submit"]'
+    extra_field_xpath = '//input[@name="Phone"]'
+    extra_field_value = random.randint(10000000, 99999999)
+    accept_terms_xpath = '//label[@for="AgreeToUseData"]/span'
+    change_trigger_key = Keys.TAB
+
+    def parse_results(self):
+        """
+        Parse website insurance offer details
+
+        :return: list of containing prices [3months, 6months, 9months, 12months]
+        """
+        response = []
+        for block in self.driver.find_elements_by_xpath('//div[@class="floatBlock2"]'):
+            part1 = block.find_element_by_xpath('.//h1')
+            part2 = block.find_element_by_xpath('.//p')
+            response.append('%s.%s' % (part1.text.replace(',', ''), part2.text.split(' ')[0]))
+        return response
