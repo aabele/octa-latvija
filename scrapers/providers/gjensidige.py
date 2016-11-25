662d4bc3a135d23818c374568144be06ea93d000
Aivis Abele <aabele@gmail.com>
2016-11-25 21:41:34 +0200

    * `Gjensidige` handler is now working.

--- scrapers/providers/gjensidige.py
+++ scrapers/providers/gjensidige.py
@@ -20,3 +20,15 @@ class GjensidigeOCTAScraper(OCTAScraper):
     car_id_xpath = '//input[@id="VehicleRegistrationNumber"]'
     passport_id_xpath = '//input[@id="VehicleRegCertificateNumber"]'
     submit_button_xpath = '//button[@class="btn next"]'
+
+    def parse_results(self):
+        """
+        Parse website insurance offer details
+
+        :return: list of containing prices [3months, 6months, 9months, 12months]
+        """
+        response = []
+        items = self.driver.find_elements_by_xpath('//div[@id="packages"]//span[@style="font-size:18px;"]')
+        for item in items:
+            response.append(item.text.split(' ')[0])
+        return response
